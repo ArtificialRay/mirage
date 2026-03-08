4773a5449083df174bf9015fc204cf2f5eec38b6
Shuaiwei Huang <shuaiweh@catalyst-fleet1.cs.cmu.edu>
2026-03-08 19:08:36 -0400

    some modification on mpk.py to support vllm integration

--- include/mirage/persistent_kernel/runtime_header.h
+++ include/mirage/persistent_kernel/runtime_header.h
@@ -28,35 +28,35 @@ constexpr int WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE = 3 * 1024;
 #endif
 
 #if defined(MODE_ONLINE_NOTOKEN) || defined(MODE_MULTI_TURN)
-  #if MPK_TARGET_CC >= 90
-  constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
-      220 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
-  // Have to be 220 for vllm compatibility, or program will stuck
-  #elif MPK_TARGET_CC >= 86
-  constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
-      99 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
-  #elif MPK_TARGET_CC >= 80
-  constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
-      160 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
-  // Have to be 160 for vllm compatibility, or program will stuck
-  #else
-  constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
-      163 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
-  #endif
+#if MPK_TARGET_CC >= 90
+constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
+    220 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
+// Have to be 220 for vllm compatibility, or program will stuck
+#elif MPK_TARGET_CC >= 86
+constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
+    99 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
+#elif MPK_TARGET_CC >= 80
+constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
+    160 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
+// Have to be 160 for vllm compatibility, or program will stuck
 #else
-  #if MPK_TARGET_CC >= 90
-  constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
-      225 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
-  #elif MPK_TARGET_CC >= 86
-  constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
-      99 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
-  #elif MPK_TARGET_CC >= 80
-  constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
-      163 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
-  #else
-  constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
-      163 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
-  #endif
+constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
+    163 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
+#endif
+#else
+#if MPK_TARGET_CC >= 90
+constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
+    225 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
+#elif MPK_TARGET_CC >= 86
+constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
+    99 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
+#elif MPK_TARGET_CC >= 80
+constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
+    163 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
+#else
+constexpr int MAX_DYNAMIC_SHARED_MEMORY_SIZE =
+    163 * 1024 - WORKER_RESERVED_STATIC_SHARED_MEMORY_SIZE;
+#endif
 #endif
 
 typedef unsigned long long int TaskId;
@@ -279,7 +279,8 @@ struct RuntimeConfig {
   int *paged_kv_indices_buffer; // Metadata for LLM serving (paged attention)
   int *paged_kv_last_page_len_buffer; // Metadata for LLM serving
   // TODO: Some resources are not used in some modes
-#if defined(MODE_OFFLINE) || defined(MODE_ONLINE) || defined(MODE_ONLINE_NOTOKEN) || defined(MODE_MULTI_TURN)
+#if defined(MODE_OFFLINE) || defined(MODE_ONLINE) ||                           \
+    defined(MODE_ONLINE_NOTOKEN) || defined(MODE_MULTI_TURN)
   int *prompt_length;     // Metadata for online/offline serving
   int *request_ids;       // Metadata for online/offline serving
   int total_num_requests; // Metadata for LLM serving
